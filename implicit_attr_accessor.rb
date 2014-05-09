76ea697efeeedefd35623a0a5aed8fa2497705ea
Joshua Schairbaum <joshua.schairbaum@gmail.com>
2014-05-09 15:16:54 -0500

    implicit attr_accessor

---
+++ implicit_attr_accessor.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+class AttrdObject
+  def method_missing(name, *args)
+    if /\w+=/.match(name)
+      self.class.send(:attr_accessor, name.to_s.gsub('=', ''))
+      send(name, *args)
+    else
+      super
+    end
+  end
+end
+
+attrd_object = AttrObject.new
+
+attrd_object.foo # NoMethodError
+
+attrd_object.foo = "bar" # bar
+
+attrd_object.foo # bar
